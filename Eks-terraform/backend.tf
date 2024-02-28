7c7e1055f95e2867f8103248403d8a6772f1121f
malamiller <121070837+malamiller@users.noreply.github.com>
2024-02-27 22:40:34 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vprofile-kops-007" # Replace with your actual S3 bucket name
+    bucket = "terrastate-project" # Replace with your actual S3 bucket name
     key    = "Jenkins-eks/terraform.tfstate"
     region = "us-east-1"
   }
